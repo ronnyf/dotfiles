123f9ab36dbe53d1d1066a726ac725e72e2ce1f3
Ronny Falk <ronnyf@icloud.com>
2024-04-29 18:39:09 -0700

    fixed tmux conf, added plugins (<C-s>)

--- stowy.sh
+++ stowy.sh
@@ -2,11 +2,12 @@
 
 target_file_name="target.stowy"
 current=`pwd`
+
 for entry in "${current}"/*; do
 	target_path="${entry}/${target_file_name}"
 	if [ -e "${target_path}" ]; then
 		package=`basename ${entry}`
-		echo "stowy package ${package} begin"
+		echo "stowy package ${package} >>> begin"
 
 		target=$(<$target_path)
 		if [ ! -d "${target}" ]; then
@@ -15,11 +16,11 @@ for entry in "${current}"/*; do
 
 		stow_target="${target}"
 		cmd="stow -t "${target}" -v ${package} --dotfiles --ignore=^target\.stowy$ --ignore=\.DS_Store"
-		echo "cmd: ${cmd}"
+		#echo "cmd: ${cmd}"
 		eval "${cmd}"
 		#result=${!cmd}
 
-		echo "stowy package ${package} done" 
+		echo "stowy package ${package} <<< done" 
 	fi
 done
 
