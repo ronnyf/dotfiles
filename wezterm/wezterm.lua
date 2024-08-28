c0c6aeef41a6659d847d0cb1f4d34160433f3275
Ronny F <ronnyf@icloud.com>
2024-08-28 11:12:21 -0700

    renamed wezterm config and moved to .config folder

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,16 +2,12 @@
 local wezterm = require("wezterm")
 
 -- This will hold the configuration.
-local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
+local config = {}
 config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("SauceCodePro Nerd Font")
 config.font_size = 15
-config.initial_cols = 144
-config.initial_rows = 44
+config.initial_cols = 134
+config.initial_rows = 34
 
 -- and finally, return the configuration to wezterm
 return config
